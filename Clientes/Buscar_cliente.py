a2c55f36cd6be69899ac35e230b9b60ffc97fb1a
ignacioBeluge <ignaciobeluge03@gmail.com>
2026-05-11 16:46:44 -0300

    Add client search functions by ID and name
    Implement functions to search for clients by ID and name.

--- Clientes/Buscar_cliente.py
+++ Clientes/Buscar_cliente.py
@@ -1,2 +1,34 @@
-def buscar_cliente():
-    print("\nFuncion pendiente: buscar cliente.")
+import json
+import os
+
+# Configuramos la ruta dinámica absoluta
+DIR_CLIENTES = os.path.dirname(os.path.abspath(__file__))
+DIR_RAIZ = os.path.dirname(DIR_CLIENTES)
+RUTA_JSON = os.path.join(DIR_RAIZ, 'clientes.json')
+
+def buscar_cliente_por_id(id_cliente):
+    """Busca un cliente específico por su ID."""
+    if not os.path.exists(RUTA_JSON):
+        return None
+        
+    with open(RUTA_JSON, 'r', encoding='utf-8') as archivo:
+        for linea in archivo:
+            if linea.strip():
+                cliente = json.loads(linea.strip())
+                if cliente["id_cliente"] == id_cliente:
+                    return cliente # Corta la ejecución apenas lo encuentra
+    return None
+
+def buscar_cliente_por_nombre(nombre_buscar):
+    """Busca clientes que coincidan parcialmente con el nombre ingresado."""
+    resultados = []
+    if not os.path.exists(RUTA_JSON):
+        return resultados
+        
+    with open(RUTA_JSON, 'r', encoding='utf-8') as archivo:
+        for linea in archivo:
+            if linea.strip():
+                cliente = json.loads(linea.strip())
+                if nombre_buscar.lower() in cliente["nombre"].lower():
+                    resultados.append(cliente)
+    return resultados
