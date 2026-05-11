04d12ae3b5eaf77af146d9c113ea08bd59cf60ac
ignacioBeluge <ignaciobeluge03@gmail.com>
2026-05-11 16:46:19 -0300

    Add function to register new clients
    Implement function to add a new client and save to JSON.

--- Clientes/Agregar_cliente.py
+++ Clientes/Agregar_cliente.py
@@ -1,2 +1,23 @@
-def agregar_cliente():
-    print("\nFuncion pendiente: agregar cliente.")
+import json
+import uuid
+import os
+
+# 1. Buscamos la carpeta actual (Clientes)
+DIR_CLIENTES = os.path.dirname(os.path.abspath(__file__))
+DIR_RAIZ = os.path.dirname(DIR_CLIENTES)
+RUTA_JSON = os.path.join(DIR_RAIZ, 'clientes.json')
+
+def agregar_cliente(nombre, domicilio, telefono, mail):
+    id_cliente = str(uuid.uuid4())[:8]
+    nuevo_cliente = {
+        "id_cliente": id_cliente,
+        "nombre": nombre,
+        "domicilio": domicilio,
+        "telefono": telefono,
+        "mail": mail
+    }
+    
+    with open(RUTA_JSON, 'a', encoding='utf-8') as archivo:
+        archivo.write(json.dumps(nuevo_cliente, ensure_ascii=False) + '\n')
+        
+    return nuevo_cliente
