447e30c3557a3d259801e74046b61c873bdfe71e
Erik <gagwaffle@gmail.com>
2026-05-18 15:25:17 -0300

    requirements ahora tiene requerimientos reales y cambie nombre de carpeta de restapi

--- Main.py
+++ Main.py
@@ -1,8 +1,6 @@
 from Menus.Menu_login import menu_login
 
-
 def main():
     menu_login()
-
 
 main()
--- Menus/Menu_login.py
+++ Menus/Menu_login.py
@@ -1,7 +1,6 @@
 from Menus.Menu_general import menu_general
 from Seguridad.Login import login
 
-
 def menu_login():
     usuario = login()
     if usuario:
