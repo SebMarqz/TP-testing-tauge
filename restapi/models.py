c110791dbb1ccae406a27582107c411af0f55cd9
erik9691 <gagwaffle@gmail.com>
2026-05-27 22:57:48 -0300

    Solucion BUG-06

--- restapi/models.py
+++ restapi/models.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional, List
+from datetime import datetime
 
 class LoginModel(BaseModel):
     username: str
@@ -7,13 +8,13 @@ class LoginModel(BaseModel):
 
 class Cliente(BaseModel):
     id: int
-    nombre: str
-    telefono: str
+    nombre: str = Field(..., min_length=1)  # Obliga a tener mínimo 1 carácter
+    telefono: str = Field(..., min_length=1)
     email: Optional[str] = None
 
 class Usuario(BaseModel):
-    username: str
-    password: str
+    username: str = Field(..., min_length=1)
+    password: str = Field(..., min_length=1)
     rol: str
 
 class Insumo(BaseModel):
@@ -28,10 +29,14 @@ class PedidoItem(BaseModel):
     cantidad: int
 
 class Pedido(BaseModel):
-    id: int = 0
+    id: int = 0  # Permite omitirlo en el POST sin fallar por Pydantic
     cliente_id: int
-    descripcion: str
-    fecha_estimada: str
+    descripcion: str = Field(..., min_length=1)
+    tipo_trabajo: str = Field(..., min_length=1)
+    fecha_estimada: str = Field(..., min_length=1)
     estado: str = "Pendiente"
     estado_pago: str = "Pendiente"
     insumos: List[PedidoItem] = []
+    fecha_creacion: str = datetime.now().strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
